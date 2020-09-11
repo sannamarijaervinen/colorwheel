b0315e10d4772bf18f394f7ff0013dae5c91f741
Sanna-Mari <69782981+sannamarijaervinen@users.noreply.github.com>
2020-09-11 09:32:08 +0300

    Color Wheel
    A program to find out what is the complementary color of the primary colors (red, blue and yellow).

--- primarycomplementarycolors.py
+++ primarycomplementarycolors.py
@@ -1,1 +1,12 @@
+# -*- coding: UTF8 -*-
 
+def colordic():
+  
+
+def main():
+  
+  primary_color = input("Insert primary color (red / blue / yellow): ")
+  
+
+if __name__ == "__main__":
+    main()
